f294865260f90785780181280c0fc93238ce0c22
Shane Logsdon <shane@shanelogsdon.com>
2016-02-28 11:15:54 -0500

    remove custom emacs.d

--- emacs.d/init.el
+++
@@ -1,8 +0,0 @@
-;;; init.el --- init script for emacs
-;;; Commentary:
-;;; Code:
-(load (concat user-emacs-directory "general.el"))
-(load (concat user-emacs-directory "packages.el"))
-(load (concat user-emacs-directory "packages-settings.el"))
-(load (concat user-emacs-directory "keymaps.el"))
-;;; init.el ends here
